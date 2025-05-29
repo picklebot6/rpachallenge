63786215678759dfdb7e8c134afb993384e8962a
Matthew Lim <dladnwp97@gmail.com>
2025-05-29 16:05:58 -0700

    added timeout of 5 minutes

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 300000,
   testDir: './main',
   /* Run tests in files in parallel */
   fullyParallel: true,
